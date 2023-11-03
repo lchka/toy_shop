416892ab0bd716b83faa5f93f5589df939f6f73a
Laura Hofmanova <N00222003@iadt.ie>
2023-11-03 23:20:42 +0000

    added some css to sort menu

--- resources/views/toys/index.blade.php
+++ resources/views/toys/index.blade.php
@@ -15,33 +15,38 @@
              This is done by requesting the user to choose a column and in which direction it would like it displayed
             this is then passed through the route (toys.index) and post the results based on the query stated there  -->
 
-            <form method="GET" action="{{ route('toys.index') }}">
-    @csrf
+                                <form method="GET" action="{{ route('toys.index') }}">
+                        @csrf
 
-    <label style="font-size:16px; background-color:#F9A8D4; padding: 8px 15px; font-weight:bold; border-radius: 5px;border-color:#F8BBD0;" for="keyword">Search by Keyword:</label>
-    <input type="text" id="keyword" name="keyword" placeholder="Type keyword" value="{{ Request::input('keyword') }}">
 
-    <label style="font-size:16px; background-color:#F9A8D4; padding: 8px 15px; font-weight:bold; border-radius: 5px;" for="column">Sort by Column:</label>
-    <select id="column" name="column">
-    <option value="name" {{ Request::input('column') == '' ? 'selected' : '' }}>Select Column</option>
-        <option value="name" {{ Request::input('column') == 'name' ? 'selected' : '' }}>Name</option>
-        <option value="colour" {{ Request::input('column') == 'colour' ? 'selected' : '' }}>Colour</option>
-        <option value="type" {{ Request::input('column') == 'type' ? 'selected' : '' }}>Type</option>
-        <option value="size" {{ Request::input('column') == 'size' ? 'selected' : '' }}>Size</option>
-    </select>
+                        
+                        <input style= "padding-right:200px; margin-right:25px;" type="text" id="keyword" name="keyword" placeholder="Search by Keyword" value="{{ Request::input('keyword') }}">
 
-    <select id="direction" name="direction">
-    <option value="name" {{ Request::input('column') == '' ? 'selected' : '' }}>Select order by</option>
-        <option value="asc" {{ Request::input('direction') == 'asc' ? 'selected' : '' }}>Ascending</option>
-        <option value="desc" {{ Request::input('direction') == 'desc' ? 'selected' : '' }}>Descending</option>
-    </select>
 
-    <button type="submit" style="background-color: #EEEEEE; color: #9E9E9E; font-weight:bold; font-size: 16px; border: #F8BBD0; padding: 6px 25px; cursor: pointer; transition: background-color 0.1s; border-radius: 5px;" 
-        onmouseover="this.style.backgroundColor='#90CAF9'; this.style.color='white'; this.style.border='2px solid #2196F3';" 
-        onmouseout="this.style.backgroundColor='#EEEEEE'; this.style.color='#9E9E9E'; this.style.border='#F8BBD0';">
-        Search & Sort
-    </button>
-</form>
+                       
+
+
+                        <select class="filter-select" style="margin-right:25px;" id="column" name="column">
+                        <option value="name" {{ Request::input('column') == '' ? 'selected' : '' }}>Select Column</option>
+                            <option value="name" {{ Request::input('column') == 'name' ? 'selected' : '' }}>Name</option>
+                            <option value="colour" {{ Request::input('column') == 'colour' ? 'selected' : '' }}>Colour</option>
+                            <option value="type" {{ Request::input('column') == 'type' ? 'selected' : '' }}>Type</option>
+                            <option value="size" {{ Request::input('column') == 'size' ? 'selected' : '' }}>Size</option>
+                        </select>
+
+                        <select class="filter-select" style="margin-right:25px;" id="direction" name="direction">
+                        <option value="name" {{ Request::input('column') == '' ? 'selected' : '' }}>Select order by</option>
+                            <option value="asc" {{ Request::input('direction') == 'asc' ? 'selected' : '' }}>Ascending</option>
+                            <option value="desc" {{ Request::input('direction') == 'desc' ? 'selected' : '' }}>Descending</option>
+                        </select> 
+
+                        <button type="submit" style="background-color: #A5D3F8; color: white; border:2px solid #2196F3; font-weight:bold; font-size: 16px; padding: 5px 20px; cursor: pointer; transition: background-color 0.1s;" 
+                            onmouseover="this.style.backgroundColor='#2295F2'; this.style.color='white'; this.style.border='2px solid #2196F3';" 
+                            onmouseout="this.style.backgroundColor='#8EC8F7'; this.style.color='white'; this.style.border='2px solid #2196F3';">
+                            Search & Sort
+                        </button>
+
+                    </form>
 
 
             <!-- used for making the succes alert appear, by saying if the session was successful then display this message -->
