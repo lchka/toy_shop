4dd46628c13bcdca51066bc29c745c7ade999f6c
Laura Hofmanova <N00222003@iadt.ie>
2023-11-27 10:46:31 +0000

    add some design to show animal admin

--- resources/views/admin/animals/show.blade.php
+++ resources/views/admin/animals/show.blade.php
@@ -1,26 +1,42 @@
 <x-app-layout>
-
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        <b>{{ ucfirst($animal->animal_name) }}</b> - Animals
+            Dashboard
         </h2>
     </x-slot>
-    <div class="py-12">
+
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <h3 class="font-bold text-2xl mb-4">Animal Details</h3>
-            <p class="text-gray-700"><span class="font-bold">Id:</span> {{ $animal->id }} </p>
-            <p class="text-gray-700"><span class="font-bold">name:</span> {{ $animal->animal_name }} </p>
-            <p class="text-gray-700"><span class="font-bold">Size:</span> {{ $animal->size }} </p>
-            <!-- might break as size is an enum -->
-            <p class="text-gray-700"><span class="font-bold">Country of Origin:</span> {{ $animal->country }} </p>
-            <x-primary-button><a href="{{ route('admin.animals.edit', $animal) }}">Edit</a></x-primary-button>
+            <!-- Used for making the success alert appear, by saying if the session was successful then display this message -->
+            <x-alert-success>
+                {{ session('success') }}
+            </x-alert-success>
+
+            <div class=" shadow-xl sm:rounded-lg">
+                <div class="my-6 p-6 border-b border-gray-200 shadow-xl sm:rounded-lg">
+
+                    <!-- Styling for Animal Details -->
+                    <div class="bg-white border-b border-gray-200 shadow-xl sm:rounded-lg p-6 mb-6">
+                        <h3 class="font-bold text-2xl mb-4">Animal Details</h3>
+                        <p class="text-gray-700"><span class="font-bold">Id:</span> {{ $animal->id }} </p>
+                        <p class="text-gray-700"><span class="font-bold">Name:</span> {{ $animal->animal_name }} </p>
+                        <p class="text-gray-700"><span class="font-bold">Size:</span> {{ $animal->size }} </p>
+                        <!-- Adjust this part based on the actual output for country -->
+                        <p class="text-gray-700"><span class="font-bold">Country of Origin:</span> {{ $animal->country }} </p>
+                         <x-primary-button><a href="{{ route('admin.animals.edit', $animal) }}">Edit</a></x-primary-button>
 
             <form method="POST" action="{{ route('admin.animals.destroy', $animal) }}">
                         @csrf
                         @method('DELETE')
                         <x-primary-button onclick="return confirm('Are you sure you want to delete?')">Delete</x-primary-button>
                     </form>
+                    </div>
+
+
+
+            
+           
 
                     
             <h3 class="font-bold text-2xl mt-6 mb-4">Toys By {{ $animal->animal_name }}</h3>
