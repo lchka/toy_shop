3dee1dda2c721ddc8e1e166f8a6110c5347341b1
Laura Hofmanova <N00222003@iadt.ie>
2023-11-27 16:45:14 +0000

    fixed filer, wasnt filtering on pagination

--- app/Http/Controllers/Admin/ToyController.php
+++ app/Http/Controllers/Admin/ToyController.php
@@ -16,46 +16,52 @@ class ToyController extends Controller
     
 
     //  the index not only displays all toys by default, it allows the user to sort through the various columns and add a query, in which. The user may input key words into the input box, select a drop down option of columns and choose in which way they want it sorted. This is done by sql querying on line 29. If the toys shown are over 5 per page, the paginate then begins to work and adds multiple page option depending on the outcome of the user sorting/input. 
-     
     public function index(Request $request)
 {
-    // Retrieve selected column and keyword for searching
-    $column = $request->input('column', ''); // Default to an empty string if no column selected
-    $keyword = $request->input('keyword', ''); // Default to an empty string if no keyword
+    $column = $request->input('column', '');
+    $keyword = $request->input('keyword', '');
+    $sortDirection = $request->input('sort_direction', 'asc');
 
-    // Initialize the query
     $toys = Toy::query();
 
-    // Search functionality for provided column and keyword
-    if (!empty($column) && in_array($column, ['name', 'colour', 'type', 'size', 'animal_name'])) {
-        // Search by 'animal_name' using relationship
-        if ($column === 'animal_name') {
-            $toys->whereHas('animal', function ($query) use ($keyword) {
-                $query->where('animal_name', 'like', '%' . $keyword . '%');
-            });
-        } else {
-            $toys->where($column, 'like', '%' . $keyword . '%');
-        }
+    if (!empty($keyword)) {
+        $toys->where(function ($query) use ($column, $keyword) {
+            if (!empty($column) && in_array($column, ['name', 'size', 'colour', 'type', 'company_name'])) {
+                // Query directly on the 'toys' table for selected column
+                $query->where($column, 'like', '%' . $keyword . '%');
+            } else {
+                $query->where('name', 'like', '%' . $keyword . '%')
+                    ->orWhere('size', 'like', '%' . $keyword . '%')
+                    ->orWhere('colour', 'like', '%' . $keyword . '%')
+                    ->orWhere('type', 'like', '%' . $keyword . '%')
+                    ->orWhere('company_name', 'like', '%' . $keyword . '%')
+                    ->orWhereHas('animal', function ($q) use ($keyword) {
+                        $q->where('animal_name', 'like', '%' . $keyword . '%')
+                            ->orWhere('breed', 'like', '%' . $keyword . '%');
+                    });
+            }
+        });
     }
 
-    // Sorting based on custom column and direction
-    $customColumn = $request->input('custom_column', ''); // Default to an empty option select
-    $customDirection = $request->input('custom_direction', ''); // Default to an empty option select
-
-    // Apply sorting only if a valid custom column is chosen
-    if (!empty($customColumn) && in_array($customColumn, ['name', 'colour', 'type', 'size', 'animal_name'])) {
-        $toys->orderBy($customColumn, $customDirection);
+    if (!empty($column) && in_array($column, ['name', 'size', 'colour', 'type', 'company_name'])) {
+        $toys->orderBy($column, $sortDirection);
+    } elseif ($column === 'animal_name' || $column === 'animal_breed') {
+        $toys->orderBy(function ($query) use ($column, $sortDirection) {
+            $query->select($column)->from('animals')->whereColumn('animals.id', 'toys.animal_id')->orderBy($column, $sortDirection);
+        });
+    } else {
+        $toys->orderBy('name', $sortDirection);
     }
 
-    // Authorize the admin to view this index
     $user = Auth::user();
     $user->authorizeRoles('admin');
 
-    // Paginate the results
     $toys = $toys->paginate(5);
 
-    return view('admin.toys.index')->with('toys', $toys); // Render the view with the paginated toys data
+    return view('admin.toys.index')->with('toys', $toys);
 }
+
+    
 
     
 
