dc7511cdab6a84938af11a70b0f85430c2374c37
Laura Hofmanova <N00222003@iadt.ie>
2023-12-04 16:20:34 +0000

    added some design changes

--- resources/views/admin/promote.blade.php
+++ resources/views/admin/promote.blade.php
@@ -2,15 +2,15 @@
 
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-900 dark:text-gray-800 leading-tight">
             {{ __('Promote User') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-pink-200 overflow-hidden shadow-xl sm:rounded-lg p-6">
-                <div class="text-lg text-gray-800 dark:text-gray-200">
+            <div class="bg-white dark:bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <div style="font-size: 22px;" class=" font-bold text-gray-800 dark:text-gray-700">
                     {{ __("Promote User to Admin") }}
                 </div>
 
@@ -18,11 +18,11 @@
                 <form action="{{ route('admin.promoteUser') }}" method="POST" class="mt-6">
                     @csrf
                     <div class="mb-4">
-                        <label for="user_id" class="block text-gray-700 dark:text-gray-300 font-bold mb-2">
+                        <label for="user_id" class="block text-gray-700 dark:text-red-300 font-bold">
                             {{ __('Select User to Promote') }}:
                         </label>
                         <div class="relative">
-                            <select name="user_id" id="user_id" class="block w-full appearance-none border dark:border-gray-600 rounded-md py-2 pl-3 pr-10 focus:outline-none focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <select name="user_id" id="user_id" class="custom-select">
                                 @foreach($users as $user)
                                     <option value="{{ $user->id }}">{{ $user->name }}</option>
                                 @endforeach
@@ -35,9 +35,9 @@
                             </div>
                         </div>
                     </div>
-                    <button type="submit" class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    <x-primary-button type="submit" class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                         {{ __('Promote to Admin') }}
-                    </button>
+                    </x-primary-button>
                 </form>
                 <!-- End of User Promotion Form -->
             </div>
