f9346d08abb82309bbad5b58b18d579c41958798
Laura Hofmanova <N00222003@iadt.ie>
2023-12-04 14:37:17 +0000

    just fixed a slight issue

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
 use Illuminate\Support\Facades\Route;
 
 // toycontroller
-use App\Http\Controllers\Admin\ToyController as AdminToyController;//aliases are used in order to make sure laravel calls for the right controller/folder
+use App\Http\Controllers\Admin\ToyController as AdminToyController; //aliases are used in order to make sure laravel calls for the right controller/folder
 use App\Http\Controllers\User\ToyController as UserToyController;
 // animalcontroller
 use App\Http\Controllers\Admin\AnimalController as AdminAnimalController;
@@ -24,18 +26,19 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
+
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
 });
 
 
-Route::resource('Admin/toys', AdminToyController::class)->middleware(['auth'])->names('admin.toys');//deleted / before admin might break
-Route::resource('User/toys', UserToyController::class)->middleware(['auth'])->names('user.toys')->only(['index','show']);
+Route::resource('Admin/toys', AdminToyController::class)->middleware(['auth'])->names('admin.toys'); //deleted / before admin might break
+Route::resource('User/toys', UserToyController::class)->middleware(['auth'])->names('user.toys')->only(['index', 'show']);
 Route::resource('Admin/animals', AdminAnimalController::class)->middleware(['auth'])->names('admin.animals');
-Route::resource('User/animals', UserAnimalController::class)->middleware(['auth'])->names('user.animals')->only(['index','show']);
+Route::resource('User/animals', UserAnimalController::class)->middleware(['auth'])->names('user.animals')->only(['index', 'show']);
 Route::resource('Admin/petstores', AdminPetstoreController::class)->middleware(['auth'])->names('admin.petstores');
-Route::resource('User/petstores', UserPetstoreController::class)->middleware(['auth'])->names('user.petstores')->only(['index','show']);
+Route::resource('User/petstores', UserPetstoreController::class)->middleware(['auth'])->names('user.petstores')->only(['index', 'show']);
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
